a8d12de920e24c1bc1f2a7bfd4caf63e1db60381
Paul Moss <vibrationoflife@protonmail.com>
2021-12-03 19:05:46 -0500

    updated xsrc now has complete methods for workign with sdk pages

--- parser/xsrc.py
+++ parser/xsrc.py
@@ -19,13 +19,14 @@ from enum import IntEnum
 import re
 logger = get_logger(Path(__file__).stem)
 
+# region SDK API Reference
 re_ln_pattern = re.compile(r"\A\s*(:?[0-9]*)\s*")
 # https://regex101.com/r/xAqRAU/1/
 re_args_pattern = re.compile(r"\s*(?:\[(\S{2,3})\])?\s*(\S*)\s(\S*)")
 # https://regex101.com/r/HU09ZZ/1
 re_method_pattern = re.compile(
     r"(\S*)\s*(?:(\S*)\()(?:\s(.*)\))|(\S*)\s*([0-9A-Z-a-z]*)")
-
+# endregion SDK API Reference
 
 @dataclass
 class ParamInfo:
@@ -33,66 +34,13 @@ class ParamInfo:
     name: str
     type: str
 
-
+# region SDK API Reference
 class CodeText(BlockObj):
+    """Responsible for getting code from url"""
+
     def __init__(self, soup: SoupObj):
         super().__init__(soup=soup)
-        self._data = """
-        /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*
-* This file is part of the LibreOffice project.
-*
-* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at http://mozilla.org/MPL/2.0/.
-*
-* This file incorporates work covered by the following license notice:
-*
-*   Licensed to the Apache Software Foundation (ASF) under one or more
-*   contributor license agreements. See the NOTICE file distributed
-*   with this work for additional information regarding copyright
-*   ownership. The ASF licenses this file to you under the Apache
-*   License, Version 2.0 (the "License"); you may not use this file
-*   except in compliance with the License. You may obtain a copy of
-*   the License at http://www.apache.org/licenses/LICENSE-2.0 .
-*/
-#ifndef __com_sun_star_awt_XWindow_idl__
-#define __com_sun_star_awt_XWindow_idl__
-#include <com/sun/star/lang/XComponent.idl>
-#include <com/sun/star/awt/Rectangle.idl>
-#include <com/sun/star/awt/XWindowListener.idl>
-#include <com/sun/star/awt/XFocusListener.idl>
-#include <com/sun/star/awt/XKeyListener.idl>
-#include <com/sun/star/awt/XMouseListener.idl>
-#include <com/sun/star/awt/XMouseMotionListener.idl>
-#include <com/sun/star/awt/XPaintListener.idl>
-module com {  module sun {  module star {  module awt {
-published interface XWindow: com::sun::star::lang::XComponent
-{
-void setPosSize( [in] long X, [in] long Y, [in] long Width, [in] long Height, [in] short Flags );
-com::sun::star::awt::Rectangle getPosSize();
-void setVisible( [in] boolean Visible );
-void setEnable( [in] boolean Enable );
-void setFocus();
-
-void addWindowListener( [in] com::sun::star::awt::XWindowListener xListener );
-
-void removeWindowListener( [in] com::sun::star::awt::XWindowListener xListener );
-void addFocusListener( [in] com::sun::star::awt::XFocusListener xListener );
-void removeFocusListener( [in] com::sun::star::awt::XFocusListener xListener );
-void addKeyListener( [in] com::sun::star::awt::XKeyListener xListener );
-void removeKeyListener( [in] com::sun::star::awt::XKeyListener xListener );
-void addMouseListener( [in] com::sun::star::awt::XMouseListener xListener );
-void removeMouseListener( [in] com::sun::star::awt::XMouseListener xListener );
-void addMouseMotionListener( [in] com::sun::star::awt::XMouseMotionListener xListener );
-void removeMouseMotionListener( [in] com::sun::star::awt::XMouseMotionListener xListener );
-void addPaintListener( [in] com::sun::star::awt::XPaintListener xListener );
-void removePaintListener( [in] com::sun::star::awt::XPaintListener xListener );
-};
-}; }; }; };
-#endif
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
-        """
+        self._data = None
 
     def get_obj(self) -> str:
         if self._data:
@@ -125,7 +73,7 @@ void removePaintListener( [in] com::sun::star::awt::XPaintListener xListener );
 class ComponentText:
     """
     gets the text of the Component area
-    
+
     Example:
         published interface XWindow: com::sun::star::lang::XComponent
         {
@@ -139,14 +87,15 @@ class ComponentText:
         void setEnable( [in] boolean Enable );
         }
     """
+
     def __init__(self, c_text: CodeText):
-        self._obj_text = c_text
+        self._code_text = c_text
         self._data = None
 
     def get_text(self) -> str:
         if self._data:
             return self._data
-        text = self._obj_text.get_obj()
+        text = self._code_text.get_obj()
         # https://regex101.com/r/xAqRAU/1/
         regex = r"published.*\s\{(\s*?.*?)*?\}"
         matches = re.search(regex, text)
@@ -154,6 +103,10 @@ class ComponentText:
         self._data = m
         return self._data
 
+    @property
+    def code_text(self) -> CodeText:
+        """Gets code_text value"""
+        return self._code_text
 
 class MethodData:
     """Gets the info for a single method"""
@@ -231,14 +184,15 @@ class MethodsText:
     Responsible for getting all lines of Component
     lines have a simple cleaning that replace :: with .
     """
+
     def __init__(self, f_text: ComponentText):
-        self._obj_text = f_text
+        self._component = f_text
         self._data = None
 
     def get_obj(self) -> List[str]:
         if self._data:
             return self._data
-        text = self._obj_text.get_text()
+        text = self._component.get_text()
         regex = r"\{\s((\s*?.*?)*?)\}"
         matches = re.search(regex, text)
         m = matches[1]
@@ -256,20 +210,28 @@ class MethodsText:
             result.append(line.replace('::', '.'))
         return result
 
+    @property
+    def component(self) -> ComponentText:
+        """Gets component value"""
+        return self._component
+
+
 class Methods:
-    def __init__(self, url:str):
+    """Iterable class that iterates through Methods and returns info"""
+
+    def __init__(self, url: str):
         self._soup = SoupObj(url=url)
-        c_text = CodeText(soup=self._soup)
-        t_component = ComponentText(c_text=c_text)
-        self._mt = MethodsText(f_text=t_component)
+        self._c_text = CodeText(soup=self._soup)
+        self._component = ComponentText(c_text=self._c_text)
+        self._mt = MethodsText(f_text=self._component)
         self._index = 0
         self._len = 0
         self._init = False
-        self._data:List[str] = None
+        self._data: List[str] = None
 
     def __iter__(self):
         return self
-    
+
     def __next__(self) -> MethodData:
         if not self._init:
             self._data: List[str] = self._mt.get_obj()
@@ -282,16 +244,175 @@ class Methods:
         md = MethodData(ln)
         self._index += 1
         return md
+
+    @property
+    def component(self) -> ComponentText:
+        """Gets component value"""
+        return self._component
+
+    @property
+    def code_text(self) -> CodeText:
+        """Gets component value"""
+        return self._c_text
+
+class Extends:
+    def __init__(self, text: ComponentText):
+        self._text = text
+        self._name = ''
+        self._init()
     
+    def _init(self):
+        regex = r"published\s*interface.*:\s*(com::.*)"
+        s = self._text.get_text()
+        matches = re.search(regex, s)
+        if matches:
+            g = matches.groups()
+            self._name = g[0].strip().replace('::', '.')
+
+    # region Properties
+    @property
+    def name(self) -> str:
+        """Gets name value"""
+        return self._name
+
+    @property
+    def component(self) -> ComponentText:
+        """Gets component object"""
+        return self._text
+    # endregion Properties
+class Imports:
+    """
+    Gets imports for interface/class etc
+    Fromat: ``com.sun.star.awt.Rectangle``
+    """
+    def __init__(self, text: CodeText):
+        self._text = text
+        self._imports: List[str] = None
+
+    def get_obj(self) -> List[str]:
+        """
+        Gets the imports as a list of strings
+        Fromat: ``com.sun.star.awt.Rectangle``
+
+        Returns:
+            List[str]: list
+        """
+        if self._imports:
+            return self._imports
+        self._imports = []
+        
+        regex = r"#include\s*<(.*)\.idl"
+        test_str = self._text.get_obj()
+        matches = re.finditer(regex, test_str)
+        for _, match in enumerate(matches, start=1):
+            g = match.groups()
+            self._process_match(g[0])
+        return self._imports
+
+    def _process_match(self, match: str):
+        # com/sun/star/lang/XComponent
+        s = match.strip()
+        parts = s.split('/')
+        ns = '.'.join(parts)
+        self._imports.append(ns)
+    # region Properties
+    @property
+    def code_text(self) -> CodeText:
+        """Gets CodeText value"""
+        return self._text
+    # endregion Properties
+class NamesSpaceInfo:
+    def __init__(self, text: CodeText):
+        self._text = text
+        self._ns = ""
+        self._ns_lst = None
+        self._init()
+
+    def _init(self) -> str:
+        text = self._text.get_obj()
+        regex = r"module (com \{.*)\{"
+        # regex = r"module com \{  module sun \{  module star \{  module awt \{"
+        matches = re.search(regex, text, flags=re.MULTILINE)
+        result = []
+        if matches:
+            result = self._process(matches[0])
+        self._ns_lst = result
+        self._ns = '.'.join(self._ns_lst)
+
+    def _process(self, input: str) -> List[str]:
+        # module com {  module sun {  module star {  module awt
+        results = []
+        parts = input.split('{')
+        for part in parts:
+            p = part.split()
+            if len(p) >= 2:
+                results.append(p[1])
+        return results
+
+    # region Properties
+    @property
+    def namespace(self) -> str:
+        """Gets namespace"""
+        return self._ns
+
+    @property
+    def parts(self) -> List[str]:
+        """Gets parts value"""
+        return self._ns_lst
+
+    @property
+    def code_text(self) -> CodeText:
+        """Gets CodeText value"""
+        return self._text
+    # endregion Properties
+
+
+class NameInfo:
+    """Gets Name of interface/class etc"""
+    def __init__(self, text: ComponentText):
+        self._text = text
+        self._name = ''
+        self._init()
+    
+    def _init(self):
+        regex = r"published\s*interface\s*(\S*):"
+        s = self._text.get_text()
+        matches = re.search(regex, s)
+        if matches:
+            g = matches.groups()
+            self._name = g[0].strip()
+    # region Properties
+    @property
+    def name(self) -> str:
+        """Gets name value"""
+        return self._name
+
+    @property
+    def component(self) -> ComponentText:
+        """Gets component object"""
+        return self._text
+    # endregion Properties
+
+# endregion SDK API Reference
+
 def main():
     os.system('cls' if os.name == 'nt' else 'clear')
-    url = 'https://api.libreoffice.org/docs/idl/ref/XWindow_8idl_source.html'
+    # url = 'https://api.libreoffice.org/docs/idl/ref/XWindow_8idl_source.html'
+    url = 'https://api.libreoffice.org/docs/idl/ref/XFont_8idl_source.html'
     m = Methods(url=url)
-    for meth in m:
-        print(meth.name)
-        print(meth.return_type)
-        print(meth.args)
- 
+    ns = NamesSpaceInfo(text=m.code_text)
+    ni = NameInfo(text=m.component)
+    im = Imports(text=m.code_text)
+    ex = Extends(text=m.component)
+    print(im.get_obj())
+    print(ni.name)
+    print("Extends:", ex.name)
+    
+    # for meth in m:
+    #     print(meth.name)
+    #     print(meth.return_type)
+    #     print(meth.args)
+    print(ns.namespace)
 
 
 if __name__ == '__main__':
