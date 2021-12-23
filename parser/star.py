e203186c4f936971a36757cb5cfc71b803fbcf15
Paul Moss <vibrationoflife@protonmail.com>
2021-12-23 08:16:38 -0500

    module links updates

--- parser/star.py
+++ parser/star.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 """
-Process link to star namesapec that contains links to modules
+Process link to star namespace that contains links to modules. Output links to json (star.json)
 """
 # region Imports
 from dataclasses import dataclass
@@ -351,8 +351,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# body > div.contents > table > tbody > tr.memitem\:namespacecom_1_1sun_1_1star_1_1accessibility > td.memItemRight > a
-
-# modules can contain other modules such as with chart2: https://api.libreoffice.org/docs/idl/ref/namespacecom_1_1sun_1_1star_1_1chart2.html
-
