11c2f4d721292e81f998d3789c6172faf710b045
Paul Moss <vibrationoflife@protonmail.com>
2022-01-26 07:38:28 -0500

    add mkdirp to WriteStarNs

--- star_ns/write_star_ns.py
+++ star_ns/write_star_ns.py
@@ -2,7 +2,7 @@
 import os
 from pathlib import Path
 from .gen_star_ns import GenerateStarNs
-from typing import Dict, Optional
+from typing import Dict, Optional, Union
 from data_manage.data_class.component import Component
 from config import AppConfig
 import logging
@@ -22,7 +22,9 @@ class WriteStarNs:
 
     def write(self) -> None:
         for ns, c_data in self._data.items():
-            write_path = self._write_root.joinpath(Path(*ns.split('.'))).joinpath('__init__.py')
+            write_path = self._write_root.joinpath(Path(*ns.split('.')))
+            self._mkdirp(write_path)
+            write_path = write_path.joinpath('__init__.py')
             gen_star = GenerateStarNs(config=self._config, c_data=c_data)
             text = gen_star.gen()
             with open(write_path, 'w') as f:
@@ -30,3 +32,17 @@ class WriteStarNs:
             if self._log:
                 rel_path = write_path.relative_to(self._root_dir)
                 self._log.info('Wrote file %s', str(rel_path))
+    
+    def _mkdirp(dest_dir: Union[str, Path]):
+        """
+        Creates directory and all child directories if needed
+
+        Args:
+            dest_dir (Union[str, Path]): path to create directories for.
+                Must be dir path only. No checking is done for file names.
+        """
+        # Python ≥ 3.5
+        if isinstance(dest_dir, Path):
+            dest_dir.mkdir(parents=True, exist_ok=True)
+        else:
+            Path(dest_dir).mkdir(parents=True, exist_ok=True)
