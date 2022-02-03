c0fd92c9e7990c49e58cccd0e81c6208fd27befd
Paul Moss <vibrationoflife@protonmail.com>
2022-02-02 19:49:52 -0500

    generated struct dynamic template

---
+++ template/_base_struct_dyn.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+import os
+from _base_struct import BaseStruct
+
+
+class BaseStructDyn(BaseStruct):
+
+    def _hydrate_data(self, json_data: dict):
+        super()._hydrate_data(json_data=json_data)
+        self.uno_obj = os.environ.get('config_uno_obj_dir', 'uno_obj')
+        self.dyn = os.environ.get('config_dyn_dir', 'dyn')
