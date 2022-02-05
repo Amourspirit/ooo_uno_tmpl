49fe5866ee4fa7a86f994139a52fd770af7c44c9
Paul Moss <vibrationoflife@protonmail.com>
2022-02-05 15:22:47 -0500

    create typedef dynamic template

---
+++ template/_base_typedef_dyn.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+import os
+from _base_typedef import BaseTypeDef
+from _base_json import EventArgs
+
+
+class BaseTypeDefDyn(BaseTypeDef):
+
+    def on_after_init_data(self, args: EventArgs) -> None:
+        super().on_after_init_data(args=args)
+        self.uno_obj = self.config.uno_obj_dir
+        self.dyn = self.config.dyn_dir
+
