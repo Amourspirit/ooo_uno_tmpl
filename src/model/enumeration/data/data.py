e96c6fd22383aa2b8a19b6c66acd0f8583c86825
Paul Moss <vibrationoflife@protonmail.com>
2022-03-05 18:27:12 -0500

    struct model and test

--- src/model/enumeration/data/data.py
+++ src/model/enumeration/data/data.py
@@ -1,17 +1,14 @@
 # coding: utf-8
 from typing import List, Union
-from ...shared.data.shared_data import BaseData, validator
+from ...shared.data.shared_data import BaseData
 from .items.data_item import DataItem
-from ...shared.data import validators
 
 class Data(BaseData):
     allow_db: bool
     quote: List[str]
     typings: List[str]
     desc: Union[List[str], str]
-    url: str
     items: List[DataItem]
-    _str_null_empty = validator('url', allow_reuse=True)(validators.str_null_empty)
     
     @property
     def fullname(self) -> str:
--- src/model/shared/data/shared_data.py
+++ src/model/shared/data/shared_data.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel, validator
 from . import validators
 
@@ -7,9 +7,10 @@ from . import validators
 class BaseData(BaseModel):
     name: str
     namespace: str
-    allow_db: bool
     url: str
     desc: List[str]
+    allow_db: bool = True
+
     _str_null_empty = validator('name','url', allow_reuse=True)(validators.str_null_empty)
     _namespace_validate = validator('namespace', allow_reuse=True)(
         validators.namespace)
