c36396261c812cc71023b3ccf77ef0670ed56dc3
Paul Moss <vibrationoflife@protonmail.com>
2022-04-13 10:49:12 -0400

    parser const pyi template

--- src/parser/const.py
+++ src/parser/const.py
@@ -1462,6 +1462,9 @@ class ConstWriter(base.WriteBase):
     def _get_template_dyn(self) -> Union[str, None]:
         return 'const_dyn.tmpl'
 
+    def _get_template_pyi(self) -> Union[str, None]:
+        return 'const_pyi.tmpl'
+
     def write(self) -> Union[str, None]:
         """
         Writes files/templates according to parameters
@@ -1486,6 +1489,7 @@ class ConstWriter(base.WriteBase):
             if self._write_file:
                 self._write_to_file()
                 self._write_to_file_dyn()
+                self._write_to_file_pyi()
             if self._write_json:
                 self._write_to_json()
             if self._json_out:
@@ -1589,6 +1593,21 @@ class ConstWriter(base.WriteBase):
         with open(write_path, 'w') as out_file:
             out_file.write(dyn_contents)
         logger.info('create file: %s', write_path)
+
+    def _write_to_file_pyi(self):
+        name = self._get_template_pyi()
+        if name is None:
+            return
+        p = self._template_dir / name
+        with open(p, 'r') as t_file:
+            contents = t_file.read()
+
+        out_file = self._file_full_path.stem + APP_CONFIG.template_pyi_ext
+        write_path = self._file_full_path.parent / out_file
+        with open(write_path, 'w') as out_file:
+            out_file.write(contents)
+        logger.info('create file: %s', write_path)
+
 
     def _write_to_json(self):
         p = self._file_full_path.parent
