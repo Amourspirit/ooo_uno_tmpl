d43f2f5cbb91c12c1be976514e94e500ec771f30
Paul Moss <vibrationoflife@protonmail.com>
2022-04-13 18:15:17 -0400

    created typedef pyi templates

--- template/_base_typedef_pyi.py
+++ template/_base_typedef_pyi.py
@@ -2,11 +2,9 @@
 from _base_typedef import BaseTypeDef
 from _base_json import EventArgs
 
-
-class BaseTypeDefDyn(BaseTypeDef):
+class BaseTypeDefPyi(BaseTypeDef):
 
     def on_after_init_data(self, args: EventArgs) -> None:
         super().on_after_init_data(args=args)
-        self.uno_obj = self.config.uno_obj_dir
-        self.dyn = self.config.dyn_dir
+
 
