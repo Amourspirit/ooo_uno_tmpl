6f02fdb62b01e6bc0952bd8f42f6c2eebafc78e3
Paul Moss <vibrationoflife@protonmail.com>
2022-05-14 15:46:29 -0400

    Const and Enum pyi not generate two files.
    Const and Enum templates do double duty.
    pyi class file is create in sun/_pyi/ subfolders for each
    const and enum.
    
    A secondary module (__init__.pyi) file is genereated in sun/ subfolder
    For const this means a module containing const names only.
    For enum this means a module with enum names only.
    
    These kinds of imports are necessary to allow const and enum values
    to be imported as single items.

--- template/_base_enum_pyi.py
+++ template/_base_enum_pyi.py
@@ -2,11 +2,17 @@
 from _base_enum import BaseEnum
 from _base_json import EventArgs
 import uno
+import os
+
 
 class BaseEnumPyi(BaseEnum):
 
     def on_after_init_data(self, args: EventArgs) -> None:
         super().on_after_init_data(args=args)
+        self.write_class = False
+        is_cls = os.environ.get("PYI_CLASS", None)
+        if is_cls is not None and is_cls.lower() in ('true', '1', 't', 'y', 'yes'):
+            self.write_class = True
 
     def get_typing(self, name: str) -> str:
         fullname = self.namespace + '.' + self.get_safe_word(self.name)
@@ -20,4 +26,3 @@ class BaseEnumPyi(BaseEnum):
         except Exception as e:
             self._lerr(e)
         return ' = ...'
-        
