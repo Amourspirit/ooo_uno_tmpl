ad65b9af3d963bb888f98b659d6851d25f962a53
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2022-07-03 17:45:54 -0400

    regen const data

--- ooobuild/dyn/i18n/native_number_mode.py
+++ ooobuild/dyn/i18n/native_number_mode.py
@@ -21,24 +21,22 @@
 from enum import IntEnum
 from typing import TYPE_CHECKING
 from ooo.oenv.env_const import UNO_ENVIRONMENT, UNO_RUNTIME
+
 _DYNAMIC = False
 if (not TYPE_CHECKING) and UNO_RUNTIME and UNO_ENVIRONMENT:
     _DYNAMIC = True
 
 if not TYPE_CHECKING and _DYNAMIC:
-    from com.sun.star.i18n import NativeNumberMode as NativeNumberMode
-    if hasattr(NativeNumberMode, '_constants') and isinstance(NativeNumberMode._constants, dict):
-        NativeNumberMode._constants['__ooo_ns__'] = 'com.sun.star.i18n'
-        NativeNumberMode._constants['__ooo_full_ns__'] = 'com.sun.star.i18n.NativeNumberMode'
-        NativeNumberMode._constants['__ooo_type_name__'] = 'const'
-    def build_enum():
-        global NativeNumberModeEnum
-        ls = [f for f in dir(NativeNumberMode) if not callable(getattr(NativeNumberMode, f)) and not f.startswith('__')]
-        _dict = {}
-        for name in ls:
-            _dict[name] = getattr(NativeNumberMode, name)
-        NativeNumberModeEnum = IntEnum('NativeNumberModeEnum', _dict)
-    build_enum()
+    from ooo.helper.enum_helper import UnoConstMeta, ConstEnumMeta
+
+    class NativeNumberMode(metaclass=UnoConstMeta, type_name="com.sun.star.i18n.NativeNumberMode", name_space="com.sun.star.i18n"):
+        """Dynamic Class. Contains all the constant values of ``com.sun.star.i18n.NativeNumberMode``"""
+        pass
+
+    class NativeNumberModeEnum(IntEnum, metaclass=ConstEnumMeta, type_name="com.sun.star.i18n.NativeNumberMode", name_space="com.sun.star.i18n"):
+        """Dynamic Enum. Contains all the constant values of ``com.sun.star.i18n.NativeNumberMode`` as Enum values"""
+        pass
+
 else:
     from ...lo.i18n.native_number_mode import NativeNumberMode as NativeNumberMode
 
