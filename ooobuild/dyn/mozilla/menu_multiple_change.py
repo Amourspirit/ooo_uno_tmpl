a1e1f3cfdb922946b2236bf2cc2fdff3c8faaadd
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-05-22 17:24:10 -0400

    regen for dyn const and struct

--- ooobuild/dyn/mozilla/menu_multiple_change.py
+++ ooobuild/dyn/mozilla/menu_multiple_change.py
@@ -22,7 +22,7 @@ import uno
 from typing import TYPE_CHECKING
 from ooo.oenv.env_const import UNO_ENVIRONMENT, UNO_RUNTIME, UNO_NONE
 if (not TYPE_CHECKING) and UNO_RUNTIME and UNO_ENVIRONMENT:
-
+    # document generators will most likely not see this.
     def _get_class():
         orig_init = None
         ordered_keys = ('Image', 'ID', 'GroupID', 'PreItemID', 'ItemText', 'IsVisible', 'IsActive', 'IsCheckable', 'IsChecked')
@@ -47,7 +47,11 @@ if (not TYPE_CHECKING) and UNO_RUNTIME and UNO_ENVIRONMENT:
     MenuMultipleChange = _get_class()
 
 else:
-    from com.sun.star.mozilla import MenuMultipleChange as MenuMultipleChange
+    if TYPE_CHECKING:
+        # keep document generators happy
+        from com.sun.star.mozilla import MenuMultipleChange as MenuMultipleChange
+    else:
+        from ...lo.mozilla.menu_multiple_change import MenuMultipleChange as MenuMultipleChange
 
 __all__ = ['MenuMultipleChange']
 
