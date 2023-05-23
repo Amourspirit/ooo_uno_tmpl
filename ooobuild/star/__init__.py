a1e3aed2420987be9598a135875575e9845bfd6a
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-05-22 20:28:37 -0400

    enum pyi files now use a protocol for typing support

--- ooobuild/star/__init__.py
+++ ooobuild/star/__init__.py
@@ -1,26 +1,11 @@
 import os
-from typing import Any, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 __version__ = "1.0.5"
-__version_tmpl__ = "0.3.0"
+__version_tmpl__ = "0.3.3"
 
 _IGNORE_IMPORT_ERROR = os.environ.get(
     "ooouno_ignore_import_error", None) in ("True", "true", "yes")
 
 if TYPE_CHECKING is False and _IGNORE_IMPORT_ERROR is False:
     raise ImportError("Are you sure that uno has been imported?")
-
-
-if TYPE_CHECKING:
-    from typing import Protocol
-else:
-    Protocol = object
-
-class UnoEnumProto(Protocol):
-    typeName: str
-    value: Any
-    
-    def __init__(self, *args, **kwargs) -> None: ...
-    def __repr__(self) -> str: ...
-    def __eq__(self, that: Any) -> bool: ...
-    def __ne__(self,other: Any) -> bool: ...
