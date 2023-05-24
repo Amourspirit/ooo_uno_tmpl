6ae1e13c6c0199595111379b4f5a69f8bb1d8627
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-05-24 08:46:35 -0400

    fix missing colon in singleton dyn template

--- tests/test_imports_dyn/test_single_import.py
+++ tests/test_imports_dyn/test_single_import.py
@@ -7,12 +7,13 @@ if __name__ == "__main__":
     pytest.main([__file__])
 from tests.import_check import ImportCheck
 
-def test_single():
+def test_single(monkeypatch):
+    monkeypatch.setenv("ooouno_ignore_runtime", "True")
     # from ooobuild.lo.document.filter_options_request import FilterOptionsRequest
     # ns = "ooobuild.lo.document.filter_options_request.FilterOptionsRequest"
     # ooobuild/dyn/chart2/x_chart_type_template.py
-    # ns = "ooobuild.lo.ucb.open_command_argument.OpenCommandArgument"
-    ns = "ooobuild.dyn.chart2.x_chart_type.XChartType"
+    ns = "ooobuild.dyn.script.cannot_convert_exception.CannotConvertException"
+    # ns = "ooobuild.dyn.chart2.x_chart_type.XChartType"
     # ns = "ooobuild.dyn.chart2.x_chart_type_template.XChartTypeTemplate"
     import uno
     imc = ImportCheck()
